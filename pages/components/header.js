08f4de5efa6a07d3a63955be540fc07c7d648b37
Annd1107 <143304880+Annd1107@users.noreply.github.com>
2025-11-04 16:00:48 +0800

    Update header.js

--- pages/components/header.js
+++ pages/components/header.js
@@ -20,12 +20,12 @@ export default function Header() {
 
           <div className="flex flex-row w-1/3 items-center gap-4">
 
-            <a href="/"><svg xmlns="http://www.w3.org/2000/svg" aria-label="Acme Store logo" viewBox="0 0 32 28" class="h-4 w-4 fill-black dark:fill-white h-[16px] w-[16px]"><path d="M21.5758 9.75769L16 0L0 28H11.6255L21.5758 9.75769Z"></path><path d="M26.2381 17.9167L20.7382 28H32L26.2381 17.9167Z"></path></svg></a>
+            <Link href="/"><svg xmlns="http://www.w3.org/2000/svg" aria-label="Acme Store logo" viewBox="0 0 32 28" class="h-4 w-4 fill-black dark:fill-white h-[16px] w-[16px]"><path d="M21.5758 9.75769L16 0L0 28H11.6255L21.5758 9.75769Z"></path><path d="M26.2381 17.9167L20.7382 28H32L26.2381 17.9167Z"></path></svg></Link>
             <h1>ACME STORE</h1>
             <div className="flex flex-row justify-evenly mx-w-full gap-4">
-              <a href="all"><p>All</p></a>
-              <a><p>Shirts</p></a>
-              <a><p>Stickers</p></a>
+              <Link href="all"><p>All</p></Link>
+              <p>Shirts</p>
+              <p>Stickers</p>
             </div>
 
           </div>
